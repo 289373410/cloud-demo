4a6a83306eb9b2eec6c1453e9392dbb09d86a1f0
289373410@qq.com <aqian3589>
2019-10-21 22:06:59 +0800

    123

--- consumer/src/main/java/com/cloud/demo/consumer/rpc/GetHello.java
+++ consumer/src/main/java/com/cloud/demo/consumer/rpc/GetHello.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "provider")
+@FeignClient(name = "provider",path = "gbq_provider")
 public interface GetHello {
 
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
